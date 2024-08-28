837c57ce9d2551dd6150e7520f0b1bba43e9d307
Nikita Polchik <polchik.nikita@gmail.com>
2024-08-28 11:34:59 -0700

    modified examples.cpp

--- examples.cpp
+++ examples.cpp
@@ -2,12 +2,10 @@
 
 std::vector<int> countPositivesSumNegatives(std::vector<int> input)
 {
-  if (input.empty())                      // .empty() to test null
+  if (input.empty())                      // .empty() to test null in any iterable object
     return {};                            // you can create empty array this way
-  int countPositives{0}, sumNegatives{0}; // double variable declaration
+  int countPositives{0}, sumNegatives{0}; // double variable declaration with "{}" instead of "="
   for (int x : input)                     // simplification of for loop
-    x > 0
-        ? countPositives++ // array with one integer can be modified directly
-        : (x != 0 ? sumNegatives += x : 0);
+    x > 0 ? countPositives++ : (x != 0 ? sumNegatives += x : 0);
   return {countPositives, sumNegatives}; // you can create noname array with variables this way
 }
--- helloworld.cpp
+++ helloworld.cpp
@@ -7,22 +7,7 @@ using std::cout;
 using std::endl;
 using std::string;
 
-void showEmpty(std::vector<int> input)
-{
-  if (input.empty())
-  {
-    cout << "Empty";
-  }
-  else
-  {
-    cout << "there is something";
-  }
-}
-
 int main()
 {
-  std::vector<int> input = {};
-  showEmpty(input);
-
   return 0;
 }
